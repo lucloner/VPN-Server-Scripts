965a1f654d77499d072adb3a6acc54dbd4a5787e
Biggee <lucloner@hotmail.com>
2019-03-22 18:05:32 +0800

    补充

---
+++ testip0.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+nslookup $1 $2 | grep -A 1 $1 | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > ns.log
+chk_res=`ls -l ns.log | awk '{ print $5 }'`
+if [ $chk_res -eq 0 ]
+then
+	rm ns.log
+else
+	mv ns.log 'ns'$1'.log'
+fi
+if [ ! -f 'ns'$1'.log' ]
+then
+	touch 'ns'$1'.log'
+else
+	cat 'ns'$1'.log'
+fi
